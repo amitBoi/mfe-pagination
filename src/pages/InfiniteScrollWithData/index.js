402aa587fb90ef2d9633427296df3962e9dd684a
Amit Bairagya <amit.bairagya@outlook.in>
2024-09-01 10:08:51 +0530

    add todos infiniscroll details

--- src/pages/InfiniteScrollWithData/index.js
+++ src/pages/InfiniteScrollWithData/index.js
@@ -1,4 +1,55 @@
-export const InfiniteScrollWithData = (props) => {
-  console.log({ props });
-  return <div>Hello!</div>;
+import { useEffect, useRef, useState } from 'react';
+import { FixedSizeList } from 'react-window';
+import { HEIGHT, LIMIT } from '@app/constants/index';
+import { ContentSkeleton } from '@app/components/ContentSkeleton';
+
+const getData = (data, startIndex, limit) => data.slice(startIndex, startIndex + limit).map(({ title }, index) => `${startIndex + index + 1}. ${title}`);
+
+export const InfiniteScrollWithData = ({ todos }) => {
+  const [items, setItems] = useState([]);
+  const observer = useRef(null);
+
+  useEffect(() => {
+    loadMoreItems(items.length, LIMIT);
+  }, []);
+
+  const loadMoreItems = (startIndex, limit) => {
+    const newItems = getData(todos, startIndex, limit);
+
+    setItems((prevItems) => [...prevItems, ...newItems]);
+  };
+
+  const lastItemRef = (element) => {
+    if (observer.current) {
+      observer.current.disconnect();
+    }
+
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        loadMoreItems(items.length, LIMIT);
+      }
+    });
+
+    if (element) {
+      observer.current.observe(element);
+    }
+  };
+
+  return (
+    <ContentSkeleton title="Todos with Infinite Scroll (data from shell-app)">
+      <FixedSizeList itemCount={items.length} itemSize={LIMIT} height={HEIGHT} width={'100%'}>
+        {({ index, style }) => {
+          if (index === items.length - 1) {
+            return (
+              <div ref={lastItemRef} style={style}>
+                {items[index]}
+              </div>
+            );
+          }
+
+          return <div style={style}>{items[index]}</div>;
+        }}
+      </FixedSizeList>
+    </ContentSkeleton>
+  );
 };
